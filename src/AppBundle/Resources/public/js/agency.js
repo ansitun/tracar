f105151300fa888d26bc00f1977d76dde67133b7
Ansuraj Khadanga <ansurajk@Ansurajs-MacBook-Air.local>
2016-04-23 02:21:39 +0530

    updated the site with changes requested by V

--- src/AppBundle/Resources/public/js/agency.js
+++ src/AppBundle/Resources/public/js/agency.js
@@ -18,9 +18,87 @@ $(function() {
 // Highlight the top nav as scrolling occurs
 $('body').scrollspy({
     target: '.navbar-fixed-top'
-})
+});
+    
+$(document).ready(function(){
+    for(var i=1; i<=10; i++) {
+        animateDiv(i);
+    }
+    
+    $(window).on("scroll",check_if_in_view);
+});
+
+
+
+function check_if_in_view() {
+      var $animation_elements = $('.timeline > li');
+      var $window = $(window);
+      var window_height = $window.height();
+      var window_top_position = $window.scrollTop();
+      var window_bottom_position = (window_top_position + window_height);
+
+      
+      
+      $.each($animation_elements, function() {
+        var $element = $(this);
+        var element_height = $element.outerHeight();
+        var element_top_position = $element.offset().top;
+        var element_bottom_position = (element_top_position + element_height);
+
+        //check to see if this current container is within viewport
+        if ((element_bottom_position >= window_top_position) &&
+            (element_top_position <= window_bottom_position)) {
+            $element.addClass('in-view');
+        } else {
+          $element.removeClass('in-view');
+        }
+      });
+      
+      return;
+}
 
 // Closes the Responsive Menu on Menu Item Click
 $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
+
+function makeNewPosition(){
+    
+    // Get viewport dimensions (remove the dimension of the div)
+    var h = $(window).height() - 50;
+    var w = $(window).width() - 50;
+    
+    var nh = Math.floor(Math.random() * h);
+    var nw = Math.floor(Math.random() * w);
+    
+    return [nh,nw];    
+    
+}
+
+function animateDiv(i){
+    var newq = makeNewPosition();
+    // Math.floor((Math.random() * 10) + 1)
+
+    var el = "#random-float-" + i;
+    var oldq = $(el).offset();
+    var speed = calcSpeed([oldq.top, oldq.left], newq);
+    
+    $(el).animate({ top: newq[0], left: newq[1] }, speed, function(){
+      animateDiv(this.id.substring(13));        
+    });
+};
+
+function calcSpeed(prev, next) {
+    
+    var x = Math.abs(prev[1] - next[1]);
+    var y = Math.abs(prev[0] - next[0]);
+    
+    var greatest = x > y ? x : y;
+    
+    var speedModifier = 0.1;
+
+    var speed = Math.ceil(greatest/speedModifier);
+
+    return speed;
+
+};
